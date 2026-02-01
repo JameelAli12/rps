bfa3fd5f7e6f244a81fa7775d0304cd34623b9b3
Jameel Ali <Jameelali0101@gmail.com>
2026-02-01 13:50:06 +0000

    removed comments

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ const playGame = () => {
     console.log(`Your Score: ${userScore}   Computer Score: ${compScore}`)
 }
 
-for(let i=1; i<=5; i++) {
-    playGame()
-}
+// for(let i=1; i<=5; i++) {
+//     playGame()
+// }
 if(userScore > compScore) {
         console.log("Congratulations! You won the game.")
     }
@@ -58,15 +58,3 @@ else {
 
 
 
-
-//when we play rps, there are two people with different minds so nobody knows what is going to be the next move of each other. 
-//in our case, there will also be two players: one computer and the other user or two users is also possible. But, for now let's think about user vs computer.
-//first of all to make it really simple, let's ask user for his move and we will give him options: r,p,s
-//and then the computer will respond randomly against the player move.
-//now there are cases to discuss about who won. 
-//other than that after the game finshes and the winner is declared the game should ask for the move again.
-
-//cases: r vs s vs p
-// r wins over s
-// s wins over p
-// p wins over r
